396c12be615417b43388a25a157a89344b084f5d
Christian Ricci <riccichristian338@gmail.com>
2024-05-18 12:09:03 -0300

    FEAT: Add Swagger Documentation

--- src/main/java/com/christian/rocketseat/Application.java
+++ src/main/java/com/christian/rocketseat/Application.java
@@ -1,9 +1,18 @@
 package com.christian.rocketseat;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Gestão de Vagas",
+				description = "API responsável pela gestão de vagas",
+				version = "1"
+		)
+)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/christian/rocketseat/security/SecurityConfig.java
+++ src/main/java/com/christian/rocketseat/security/SecurityConfig.java
@@ -20,6 +20,12 @@ public class SecurityConfig {
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources"
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
@@ -27,6 +33,7 @@ public class SecurityConfig {
                     auth.requestMatchers("/candidate").permitAll()
                             .requestMatchers("/company/").permitAll()
                             .requestMatchers("/company/auth").permitAll()
+                            .requestMatchers(SWAGGER_LIST).permitAll()
                             .requestMatchers("/candidate/auth").permitAll();
                     auth.anyRequest().authenticated();
                 })
